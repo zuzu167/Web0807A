7570671fcb43591018920c0304bacbf577f6664d
zuzu167 <81228002+zuzu167@users.noreply.github.com>
2021-03-23 16:52:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Webt</title>
+</head>
+<body>
+    <section>
+        <article>
+            <h1>Inspection</h1>
+            <p>Mirum est notare quam littera gothica,<br>quam nunc putamus parum claram</p>
+            <div class="price">$300</div>
+            <ul>
+                <li><a href="#">Auto product Liability</a></li>
+                <li><a href="#">Crib Recall</a></li>
+                <li><a href="#">Defective Airbags</a></li>
+                <li><a href="#">DefectiveMedical Device</a></li>
+                <li><a href="#">Food Poisoning</a></li>
+            </ul>
+        </article>
+        <article>
+            <h1>Diagnostic</h1>
+            <p>Mirum est notare quam littera gothica,<br>quam nunc putamus parum claram</p>
+            <div class="price">$400</div>
+            <ul>
+                <li><a href="#">Auto product Liability</a></li>
+                <li><a href="#">Crib Recall</a></li>
+                <li><a href="#">Defective Airbags</a></li>
+                <li><a href="#">DefectiveMedical Device</a></li>
+                <li><a href="#">Food Poisoning</a></li>
+            </ul>
+        </article>
+        <article>
+            <h1>Upgrades</h1>
+            <p>Mirum est notare quam littera gothica,<br>quam nunc putamus parum claram</p>
+            <div class="price">$500</div>
+            <ul>
+                <li><a href="#">Auto product Liability</a></li>
+                <li><a href="#">Crib Recall</a></li>
+                <li><a href="#">Defective Airbags</a></li>
+                <li><a href="#">DefectiveMedical Device</a></li>
+                <li><a href="#">Food Poisoning</a></li>
+            </ul>
+        </article>
+    </section>
+</body>
+</html>
